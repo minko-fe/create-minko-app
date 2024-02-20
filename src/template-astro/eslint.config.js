b0141e84f19622c0ad13174decb2b6614cd20d6d
minko <49073282+hemengke1997@users.noreply.github.com>
2024-02-20 11:06:33 +0800

    Revert "feat: add astro template"

--- src/template-astro/eslint.config.js
+++
@@ -1,5 +0,0 @@
-import { createRequire } from 'node:module'
-const require = createRequire(import.meta.url)
-
-const { defineConfig } = require('@minko-fe/eslint-config')
-export default defineConfig([])
